403835caac837d16a6c284799a083b35b39f1acb
holasojo <joso930115@gmail.com>
2017-12-01 11:37:06 -0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    // return (
+    //   <div className="App">
+    //     <h1>hello</h1>
+    //   </div>
+    // );
+    return React.createElement('div', null, 'h1','react app!!!')
+  }
+}
+
+// hello this is
+export default App;
